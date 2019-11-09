13a3c2e1bfb8f1dd529710145a6ef00a40d9674d
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-09 21:56:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,9 +15,9 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
-    elsif side1 == side2 && side1 == side3
+    elsif @side1 == @side2 && @side1 == @side3
       :equilateral
-    elsif side1 == side2 || side1 == side3 || side2 == side3
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       :isosceles
     else
       :scalene
@@ -33,9 +33,9 @@ class Triangle
   
     
   def invalid?
-    if side1 <= 0 || side2 <= 0 || side3 <= 0
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       true
-    elsif side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2
+    elsif @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
       true
     else
       false
