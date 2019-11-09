e9d572237820b681ff42773b16f77201657528db
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-09 22:03:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,7 +28,7 @@ class Triangle
   def invalid?
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       true
-    elsif @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
       true
     else
       false
