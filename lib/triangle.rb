f09546fd223fabdb5bd90a2e0e28a1a3581d6018
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-09 21:50:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,12 @@ class Triangle
   end
   
   def kind
-    if self.valid?
-    
+    if self.invalid?
+      begin
+      raise TriangleError 
+      rescue TriangleError => error
+        puts error.message
+      end
     elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
@@ -19,13 +23,13 @@ class Triangle
       
     end
     
-  def valid?
+  def invalid?
     if side1 == 0 || side2 == 0 || side3 == 0
-      false
-    elsif side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2
-      false
-    else
       true
+    elsif side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2
+      true
+    else
+      false
     end
   end
   
