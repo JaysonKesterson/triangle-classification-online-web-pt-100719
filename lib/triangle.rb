37d91b74f13b6fef0677966b47e09d47426e28b1
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-09 21:35:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,16 @@
 class Triangle
   
-  
+  attr_reader :side1, :side2, :side3
   
   def initialize(side1,side2,side3)
     @side1 = side1
-    
+    @side2 = side2
+    @side3 = side3
   end
   
-  
+  def kind
+    
+  end
   
   
   
